0251ac07fb7952e8ee04972961b9e153ce0e4388
Joshua <Joshua@KE-HQ-JOSHUA.M-KOPA.local>
2018-03-19 09:45:31 +0300

    Csharp Learning

---
+++ Controllers/AppController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CsharpLearning.Controllers
+{
+    public class AppController:Controller
+    {
+        public IActionResult Index()
+        {
+            //throw new InvalidOperationException("Bad things happened");
+            return View();
+        }
+        [HttpGet("contacts")]
+        public IActionResult Contacts()
+        {
+            ViewBag.Title = "Contact US";
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewBag.Title = "About US";
+            return View();
+        }
+    }
+}
